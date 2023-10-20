08e2feb56fb2e43a23c6945c2d2789f98d6f238f
Icaro <icaroxenofonte@ciandt.com>
2023-10-20 17:54:10 -0300

    Melhoras no código seguindo as melhores práticas

--- Brasileirao.py
+++ Brasileirao.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-def pretty_json(json_data): # define o Json_pretty
+def pretty_json(json_data): # Imprime um objeto JSON formatado de forma legível.
     print(json.dumps(json_data, indent=4))
 
-def BRASILEIRAO_TABELA(): #define a função da tabela do campeonato brasileiro
+def brasileirao_tabela(): #define a função da tabela do campeonato brasileiro
     URL = 'https://api.api-futebol.com.br/v1/campeonatos/10/tabela'
     token = 'SuaChaveDeAPIAqui'
     headers = {'Authorization': f'Bearer {token}'}
@@ -15,9 +15,9 @@ def BRASILEIRAO_TABELA(): #define a função da tabela do campeonato brasileiro
         pretty_json(data)
         #print(data)
     else:
-        print(f"A solicitação GET falhou com o código de resposta {response.status_code}")
+        print(f"A solicitação GET falhou com o código de resposta {response.status_code}") # lança exceção se o status code for diferente de 200
 
-def BRASILEIRAO_ARTILHARIA(): #define a função da artilharia do campeonato brasileiro
+def brasileirao_artilharia(): #define a função da artilharia do campeonato brasileiro
      URL = 'https://api.api-futebol.com.br/v1/campeonatos/10/artilharia'
      token = 'SuaChaveDeAPIAqui'
      headers = {'Authorization': f'Bearer {token}'}
@@ -34,8 +34,8 @@ print("Bem-vindo ao Script do BRASILEIRÃO")
 user_input = input("Digite 1 para TABELA e 2 para ARTILHARIA: ")
 
 if user_input == '1':
-    BRASILEIRAO_TABELA()
+    brasileirao_tabela()
 elif user_input == '2':
-    BRASILEIRAO_ARTILHARIA()
+    brasileirao_artilharia()
 else:
     print("Valor inválido. Digite 1 ou 2")
